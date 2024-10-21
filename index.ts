a52259ab416db9384dee95ec45f3d842959e989b
besoeasy <besoeasy@users.noreply.github.com>
2024-10-21 14:00:23 +0530

    Refactor domain length and keyword in main function

--- index.ts
+++ index.ts
@@ -1,20 +1,17 @@
 import { generateDomains } from "./x/generateDomains";
 import { checkDomain } from "./x/checkDomain";
+import config from "./config.json";
 
 const file = Bun.file("domains.txt");
 const writer = file.writer();
 
-const length = 4;
-const keyword = "aa";
-const domain_extension = ".com";
-
 async function main() {
-  const domains = generateDomains(length, keyword);
+  const domains = generateDomains(config.length, config.keyword);
 
   console.log("Total Possible Domains : " + domains.length);
 
   for await (const element of domains) {
-    const domain = element + domain_extension;
+    const domain = `${element}.${config.extension}`;
 
     const addrs = await checkDomain(domain);
 
